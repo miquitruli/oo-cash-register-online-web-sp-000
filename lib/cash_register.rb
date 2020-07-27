40d90b559c7d116c6437bd146e22b5e3938a85e3
MICAELLA COLOMBO <micaelc@g.clemson.edu>
2020-07-27 23:59:02 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,32 @@
+class CashRegister
+  attr_accessor :total, :discount, :items, :price
+  def initialize(discount=0)
+    @total=0
+    @discount=discount
+    @items= []
+  end
 
+  def add_item(item, price, quantity = 1)
+    @total += price * quantity
+    quantity.times do
+      items << item
+    end
+    @price = price * quantity
+  end
+
+  def apply_discount
+    if @discount == 0
+      return "There is no discount to apply."
+    elsif @total -= @total * @discount/100
+      return "After the discount, the total comes to $#{@total}."
+    else
+      nil
+    end
+  end
+
+  def void_last_transaction
+    @total = @total-@price
+  end
+
+
+end
