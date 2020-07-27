140e97a6c11dc7c333b8d0051c59c1532d293fbc
MICAELLA COLOMBO <micaelc@g.clemson.edu>
2020-07-27 23:52:21 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -31,9 +31,7 @@ class CashRegister
   end
 
   def void_last_transaction
-    def void_last_transaction
-    self.total = self.total - self.last_transaction
-  end
+    
     @total = @total-@price
   end
 
